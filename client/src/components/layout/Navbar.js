713521303a50c735cf0d0d9b390f600cb162b33d
David Murphy <dmurphy4@molloy.edu>
2019-08-13 16:05:00 -0400

    Setup login and register components.

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,15 +1,16 @@
 import React from 'react'
+import {Link} from 'react-router-dom' 
 
 const Navbar = () => {
   return (
     <nav className="navbar bg-dark">
       <h1>
-        <a href="index.html"><i className="fas fa-code"></i> DevConnector</a>
+        <a href="index.html"><i className="fas fa-code"></i> DevNetwork</a>
       </h1>
       <ul>
         <li><a href="profiles.html">Developers</a></li>
-        <li><a href="register.html">Register</a></li>
-        <li><a href="login.html">Login</a></li>
+        <Link to="/register">Register</Link>
+        <Link to="/login">Login</Link>
       </ul>
     </nav>
   )
